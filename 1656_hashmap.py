b9dc8e4cfd5208e3608dacf63268ffa4c4ca6f83
Anassidr <kamalanass96@gmail.com>
2023-03-27 14:40:12 +0200

    python sol

---
+++ 1656_hashmap.py
@@ -0,0 +1,18 @@
+# problem 1656 Design an Ordered Stream
+
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.map = {}
+        self.n = n
+        self.i = 1
+        
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.map[idKey] = value
+        chunk = []
+        while self.i in self.map:
+            chunk.append(self.map[self.i])
+            self.i += 1
+        return chunk
+    
