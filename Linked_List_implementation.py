5d4379e1701e533fe2e61b2de382bf4107f190d1
Anassidr <kamalanass96@gmail.com>
2023-04-26 14:34:45 +0200

    linked lists

---
+++ Linked_List_implementation.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def countNodes(self, node):
+        if node:
+            self.count += 1
+            return self.countNodes(node.next)
+        else:
+            return self.count
+
+node_a = Node(5)
+node_b = Node(3)
+node_a.next = node_b
+
+mylist = LinkedList()
+mylist.head = node_a 
+
+print(mylist.countNodes(node_a))
