120dfcff0aca0dabb8a1c443a3fca73d2a0ea249
Anassidr <kamalanass96@gmail.com>
2023-05-05 15:45:29 +0200

    0505

---
+++ Linked_Lists/Leetcode_141_LinkedListCycle.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# approach : use hashmap. 
+# Complexity O(n) time and O(n) space
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        mymap = {}
+        while head:
+            if head.next in mymap:
+                return True
+            mymap[head] = True
+            head = head.next
+        return False
+    
+
+# pretty clever approach
+# Using two pointers fast and slow 
+# They will eventually meet because the distance between them is reduced by 1 at each iteration
+# O(n) time and O(1) space
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
