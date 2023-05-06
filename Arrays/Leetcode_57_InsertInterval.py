f37a916339f428d43e067b4df4cf4ef867fd87b8
Anassidr <kamalanass96@gmail.com>
2023-05-06 17:31:45 +0200

    0605

--- Arrays/Leetcode_57_InsertInterval.py
+++ Arrays/Leetcode_57_InsertInterval.py
@@ -6,9 +6,12 @@ class Solution:
                 res.append(newInterval)
                 return res + intervals[i:]
             elif newInterval[0] > intervals[i][1]:
+                # if the current interval doesn't overlap at all, just add it and keep going
                 res.append(intervals[i])
             else:
+                # newInterval keeps being overwritten until it finds its spot, then we add it and return 
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-            
+
+        # in case newinterval belongs at the end and we didn't add it yet 
         res.append(newInterval)
         return res
